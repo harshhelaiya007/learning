2783b2d7a43a7ba7926ffd45b940199cdc31b65d
harshhelaiya <harsh.helaiya@teknopoint.in>
2024-06-04 13:02:24 +0530

    wip

--- Databases/index.js
+++ Databases/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://admin:fmD4IdGmyY4Q5S0f@cluster0.cngehgz.mongodb.net/userappnew")
+
 
 const newUser = mongoose.model('Users',
     {
